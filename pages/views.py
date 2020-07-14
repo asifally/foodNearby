8569eca966935b4193bd3f82f15b28b2c0be3c57
asifally <asifally004@gmail.com>
2020-07-14 17:41:05 -0500

    started creating the backend and some html

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 
 # Create your views here.
+class Geoform(forms.Form):
+    Longitude = forms.CharField(max_length = 50)
+    Latitude = forms.CharField(max_length = 50)
+    Distance = forms.CharField(max_length = 50)
+
+def index(request):
+    if request.method == "GET":
+        return render(request, 'pages/index.html')
+    elif request.method == "POST":
+        form = GeoForm(request.POST)
+        if form.is_valid():
+            return render(request, 'pages/places.html', {'form' : form})
+
+def places(request):
+    return render(request, 'pages/places.html')
