c8e17832d983d079260e4b907d5103e3d99b01c1
asifally <asifally004@gmail.com>
2020-07-14 13:53:21 -0500

    Creation of food nearby project

---
+++ foodNearby/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for foodNearby project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodNearby.settings')
+
+application = get_asgi_application()
